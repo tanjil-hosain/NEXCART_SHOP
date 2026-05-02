315ad9e48b4b299ab859a885d745cfc5f10dfda9
tanjil-hosain <tanjilh85@gmail.com>
2026-05-03 01:08:27 +0600

    fb-connection

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+// Database connection
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASS = '';
+$DB_NAME = 'nexcart';
+
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+if ($conn->connect_error) {
+    die('DB Connection failed: ' . $conn->connect_error);
+}
+$conn->set_charset('utf8mb4');
